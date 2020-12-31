95d058d4ac7ad50338244f96a700f35f357bd7a1
Yucheng Zhou <zhouyucheng98@outlook.com>
2020-12-31 20:11:36 +0800

    update clean for runs

--- src/clean.ps1
+++ src/clean.ps1
@@ -1,2 +1,2 @@
-dir ./runs -File -Recurse -Exclude *.ps1 | ?{$_.Length -lt 1000} | ri # delete files that size < 1KB
-dir ./runs -Directory | ?{(dir $_ | measure).Count -eq 0} | ri # delete empty folders
+dir ./runs -File -Recurse | ?{$_.Length -lt 1000} | %{write "rm: $_"; ri $_.FullName} # delete files that size < 1KB
+dir ./runs -Directory | ?{(dir $_.FullName | measure).Count -eq 0} | %{write "rmdir: $_"; ri $_.FullName} # delete empty folders
