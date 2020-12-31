c7aa02971b75e0da54fad465d23b552737261fc5
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-01 02:24:51 +0800

    update clean limit

--- src/clean-runs.ps1
+++ src/clean-runs.ps1
@@ -1,3 +1,3 @@
 cd $PSScriptRoot
-dir ./runs -File -Recurse -Exclude *.ps1 | ?{$_.Length -lt 1000} | %{write "rm: $_"; ri $_.FullName} # delete files that size < 1KB
+dir ./runs -File -Recurse -Exclude *.ps1 | ?{$_.Length -lt 30000} | %{write "rm: $_"; ri $_.FullName} # delete files that size < 1KB
 dir ./runs -Directory | ?{(dir $_.FullName | measure).Count -eq 0} | %{write "rmdir: $_"; ri $_.FullName} # delete empty folders
