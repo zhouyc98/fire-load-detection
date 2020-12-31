a38e6b1abd8bee142ec5ab1ea724a07f379b0813
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-01 01:52:35 +0800

    move runs clean.ps1

--- src/clean.ps1
+++
@@ -1,2 +0,0 @@
-dir ./runs -File -Recurse | ?{$_.Length -lt 1000} | %{write "rm: $_"; ri $_.FullName} # delete files that size < 1KB
-dir ./runs -Directory | ?{(dir $_.FullName | measure).Count -eq 0} | %{write "rmdir: $_"; ri $_.FullName} # delete empty folders
