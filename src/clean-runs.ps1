a0589f3b65486cd510f8af7bd5bfadb8b8862c61
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-01 15:49:47 +0800

    fix write output

--- src/clean-runs.ps1
+++ src/clean-runs.ps1
@@ -1,3 +1,5 @@
 cd $PSScriptRoot
-dir ./runs -File -Recurse -Exclude *.ps1 | ?{$_.Length -lt 30000} | %{write "rm: $_"; ri $_.FullName} # delete files that size < 1KB
-dir ./runs -Directory | ?{(dir $_.FullName | measure).Count -eq 0} | %{write "rmdir: $_"; ri $_.FullName} # delete empty folders
+echo 'Note: donot use this script when other GPUs are running'
+dir ./runs -File -Recurse -Exclude *.ps1 | ?{$_.Length -lt 30000} | %{echo "rm: $_"; ri $_.FullName} # delete files that size < 1KB
+dir ./runs -Directory | ?{(dir $_.FullName | measure).Count -eq 0} | %{echo "rmdir: $_"; ri $_.FullName} # delete empty folders
+echo 'Done.'
