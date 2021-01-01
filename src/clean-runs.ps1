e5e93e14b5f07e4c789c5c59cb543e795b980df9
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-01 17:16:21 +0800

    update rm size limit

--- src/clean-runs.ps1
+++ src/clean-runs.ps1
@@ -1,16 +1,4 @@
 cd $PSScriptRoot
 
-$fs=dir ./runs -File -Recurse -Exclude *.ps1 | ?{$_.Length -lt 30000} # files that size < 3KB
-$ds=dir ./runs -Directory | ?{(dir $_.FullName | measure).Count -eq 0} # empty folders
-
-if($fs -and $ds){
-	$fs | %{echo "rm:    $_"}
-	$ds | %{echo "rmdir: $_"}
-
-	$confirmation = Read-Host "`nSure to delete (donot delete running files)? y/[n]"
-	if ($confirmation -eq 'y') {
-		$fs | ri
-		$ds | ri
-		echo 'Done.'
-	}
-}
+dir ./runs -File -Recurse -Exclude *.ps1 | ?{$_.Length -lt 10kb} |   %{echo "rm:    $_"; ri $_} # files that size < 10KB
+dir ./runs -Directory | ?{(dir $_.FullName | measure).Count -eq 0} | %{echo "rmdir: $_"; ri $_} # empty folders
