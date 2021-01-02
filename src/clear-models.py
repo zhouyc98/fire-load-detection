19b9600ad4cb8be5c88083a6cf4d817f94a969cc
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-02 20:23:53 +0800

    add clear-models

---
+++ src/clear-models.py
@@ -0,0 +1,23 @@
+import argparse
+import glob
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--cuda', type=str, default='0', help='cuda visible device id')
+    parser.add_argument('-t', '--ap_thr', type=float, default=24, help='ap threshold to clear')
+    
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    fns=glob.glob(f'./output{args.cuda}/*-ap*.pth')
+    n_clean=0
+    for fn in fns:
+        ap=float(fn.split('-ap')[1][:4])
+        if ap<args.ap_thr:
+            with open(fn, 'r+') as fp:
+                fp.truncate()
+            n_clean+=1
+    
+    print(f'Clear {n_clean} models')
