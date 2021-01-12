555fcb9c371d35bcac5c1a39f26e829dd9cbee2d
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-12 21:38:51 +0800

    do not run train.py automatically

--- src/test-train.sh
+++ src/test-train.sh
@@ -3,10 +3,8 @@
 echo 'Init & clear tests/'
 mkdir ../tests/ 2> /dev/null
 cp -f ./*.py ../tests/
-rm -r ../tests/output0/ 2> /dev/null
+rm -r ../tests/output/ 2> /dev/null
 rm -r ../tests/output1/ 2> /dev/null
 
-echo 'Test: train.py'
 cd ../tests
-pwd
-python3.7 train.py -n R50 -b 1
+# python3.7 train.py -n R50 -b 1 -it 1k
